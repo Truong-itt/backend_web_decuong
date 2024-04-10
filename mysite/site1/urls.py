fa2571f4f3e7529fbdf7de92a8e9342163cfea92
BapPine <baphongpine@gmail.com>
2024-04-10 23:23:26 +0700

    update gitignore

--- mysite/site1/urls.py
+++ mysite/site1/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import *
 
 urlpatterns = [
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    
     path('curriculums/', CurriculumListView.as_view(), name='Curriculum'),
     path('curriculums/<str:pk>', CurriculumDetailView.as_view(), name='Curriculum'),
     path('curriculums_delete_child/<str:pk>', CurriculumDeleteChildView.as_view(), name='Curriculumm'),
--- mysite/site1/views.py
+++ mysite/site1/views.py
@@ -6,6 +6,25 @@ from django.http import Http404
 from rest_framework import status
 from rest_framework.views import APIView
 
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Your API",
+      default_version='v1',
+      description="Description of your API",
+      terms_of_service="https://www.example.com/policies/terms/",
+      contact=openapi.Contact(email="contact@example.com"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
+
+
 class UserCreateAPIView(APIView):
     def post(self, request, *args, **kwargs):
         serializer = UserSerializer(data=request.data)
